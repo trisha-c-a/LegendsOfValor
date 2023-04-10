9ae8551046d09b158f19ab7878772ee68ad6b050
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-10 18:13:27 -0400

    Update Bush.java

--- Bush.java
+++ Bush.java
@@ -2,4 +2,17 @@ public class Bush extends Cell{
     public Bush(String name){
         super(name);
     }
+    public void entry(Hero h) {
+        System.out.println("You have entered a cave");
+        System.out.println("Increasing your dexterity to aid your battles....");
+        h.setDexterityValue(h.getDexterityValue()*1.1);
+        System.out.println("Updated dexterity: " + h.getDexterityValue());
+    }
+
+    public void exit(Hero h) {
+        System.out.println("You have exited the cave");
+        System.out.println("Resetting dexterity....");
+        h.setDexterityValue(h.getDexterityValue()*0.9);
+        System.out.println("Updated dexterity: " + h.getDexterityValue());
+    }
 }

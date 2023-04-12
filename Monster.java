e5e01d237af22bff0b8fa58dcd7b067090b610bc
Trisha Anil <trishaviking@gmail.com>
2023-04-11 22:45:41 -0400

    Modified takeAction

--- Monster.java
+++ Monster.java
@@ -40,44 +40,7 @@ public class Monster extends Character{
             System.out.println(this.getName() + " has attacked " + h.getName() + " for " + this.baseDamage + " damage!");
         }
     }
-    
-    public void takeAction(HeroGroup heroGroup){
-        int currentX = this.getCurrPos().get(0);
-        int currentY = this.getCurrPos().get(1);
-        if(this.monsterValidMove(heroGroup)){
-            currentX -= 1;
-            this.setCurrPos(Arrays.asList(currentX,currentY));
-        }else{
-            Hero hero = this.heroInRange(heroGroup);
-            if(hero != null){
-                this.attackHero(hero);
-            }
-        }
-    }
 
-                                            //GETTERS AND SETTERS
-    public double getBaseDamage(){
-        return this.baseDamage;
-    }
-
-    public double getDefenseValue(){
-        return this.defenseValue;
-    }
-
-    public double getDodgeAbility(){
-        return this.dodgeAbility;
-    }
-
-    public void setBaseDamage(double bD){
-        this.baseDamage = bD;
-    }
-    public void setDefenseValue(double dV){
-        this.defenseValue = dV;
-    }
-    public void setDodgeAbility(double dA){
-        this.dodgeAbility = dA;
-    }
-    
     public boolean monsterValidMove(HeroGroup HeroGroup){
         for(int i = 0 ; i < HeroGroup.getNumberOfHeros(); i++){
             if(this.getCurrPos().get(0).equals(HeroGroup.getPack().get(i).getCurrPos().get(0)) && this.getCurrPos().get(1).equals(HeroGroup.getPack().get(i).getCurrPos().get(1))){
@@ -106,5 +69,28 @@ public class Monster extends Character{
             }
         }
         return null;
+    }
+
+                                            //GETTERS AND SETTERS
+    public double getBaseDamage(){
+        return this.baseDamage;
+    }
+
+    public double getDefenseValue(){
+        return this.defenseValue;
+    }
+
+    public double getDodgeAbility(){
+        return this.dodgeAbility;
+    }
+
+    public void setBaseDamage(double bD){
+        this.baseDamage = bD;
+    }
+    public void setDefenseValue(double dV){
+        this.defenseValue = dV;
+    }
+    public void setDodgeAbility(double dA){
+        this.dodgeAbility = dA;
     }
 }

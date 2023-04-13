2a482bc3cb82c6186e165864bc8e7e7a5a937048
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-13 16:45:10 -0400

    Update Common.java

--- Common.java
+++ Common.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Common extends Cell{
+
+    private ArrayList<String> characters;
     public Common(String name) {
         super(name);
+        characters = new ArrayList<String>();
+        characters.add(" ");
+        characters.add(" ");
     }
+
+    public void entry(Hero h) {
+        this.characters.set(0, String.valueOf(h.getName().charAt(0)));
+    }
+
+    public void exit(Hero h) {
+        this.characters.set(0, " ");
+    }
+
+    public void addChar(Monster m){
+        this.characters.set(1, String.valueOf(m.getName().charAt(0)));
+    }
+
+    public void removeChar(Monster m){
+        this.characters.set(1, " ");
+    }
+    public void printCell(){
+        System.out.println("P  P  P  P");
+        System.out.println("P  " + this.characters.get(0) + "  " + this.characters.get(1) + "  P");
+        System.out.println("P  P  P  P");
+    }
+
+    public void printFirst(){System.out.print("P  P  P  P");}
+    public void printSecond(){System.out.print("P  " + this.characters.get(0) + "  " + this.characters.get(1) + "  P");}
+    public void printThird(){System.out.print("P  P  P  P");}
 
 }

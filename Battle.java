8ee8c3f05025443b6908a62e89fc6c2d7009cc45
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:08:45 -0400

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,4 +1,5 @@
 public interface Battle {
+    // ensures the classes that implements this has the ability to start fight and check winning condition
 
     public boolean Fight(HeroGroup heroes);
 

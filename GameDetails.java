7edd6f16d24a83acaf5ae687810b453ac44afcda
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:29:35 -0400

    Update GameDetails.java

--- GameDetails.java
+++ GameDetails.java
@@ -1,4 +1,7 @@
 public interface GameDetails {
+    
+    //This interface ensures the flow of the game on classes that implement it.
+    //All games that implements this interface must contain the following three methods to ensure a good flow on gaming experience
 
     public void rules();
     public void playGame();

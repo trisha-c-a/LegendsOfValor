4fbff941f66b882fda7176a8762e2e6279c73f43
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:44:02 -0400

    Update Sorceror.java

--- Sorceror.java
+++ Sorceror.java
@@ -1,6 +1,8 @@
 import java.util.List;
 
 public class Sorceror extends Hero implements FavoredSkill{
+    //Creates a Sorceror hero instance
+    //Ensures the attributes are set correctly when leveled up
 
     public Sorceror(String displayName, String nme, String type, String lane, List<Integer> position, double mp, long exp, double sVal, double dexVal, double agVal, double gAmt){
         super(displayName, nme, type, lane, position, mp, exp, sVal, dexVal, agVal,gAmt);

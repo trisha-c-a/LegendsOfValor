a69549752393ef7cb27d031bd6fe9183e1e6eddf
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:39:05 -0400

    Update ModifyAttribute.java

--- ModifyAttribute.java
+++ ModifyAttribute.java
@@ -1,4 +1,5 @@
 public interface ModifyAttribute {
+    //This class ensures that all celss that involve update attribute during entry and exit implement the following two methods
 
     public void entry(Hero h);
     public void exit(Hero h);

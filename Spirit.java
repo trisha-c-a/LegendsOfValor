4c4fa6cdaa4a45d99fe08ef03968f32d4ce5b0d4
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:49:21 -0400

    Update Spirit.java

--- Spirit.java
+++ Spirit.java
@@ -1,4 +1,6 @@
 public class Spirit extends Monster implements FavoredSkill {
+    //Creates instance of monster of type Spirit
+    //Includes method to setup the base dodge chance of the monster
     public void updateSkill() {
         super.setDodgeAbility(super.getDodgeAbility()*1.05);
     }

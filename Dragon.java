2abeac1d4e28e77549b1f3408735f228c757d0bf
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:23:16 -0400

    Update Dragon.java

--- Dragon.java
+++ Dragon.java
@@ -1,4 +1,6 @@
 public class Dragon extends Monster implements FavoredSkill{
+    
+    //Sets the base damage for the monster type dragon
 
     public void updateSkill() {
         super.setBaseDamage(super.getBaseDamage()*1.05);

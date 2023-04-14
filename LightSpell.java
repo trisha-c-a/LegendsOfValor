8c9e2001e6d17a9f13766c12c4581bd3c4ebebe9
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:27:47 -0400

    Update LightSpell.java

--- LightSpell.java
+++ LightSpell.java
@@ -1,4 +1,6 @@
 public class LightSpell extends Spell{
+    //Creates an instance of FireSpell item
+    //Provides function to allow for usage of the spell
     public LightSpell(String n, double p, long l, double d, double mp) {
         super(l, p, n, "Light Spell", d, mp);
     }

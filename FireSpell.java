534f0cd7ee2969ee78a2dcdd5dff84c0730ddb32
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:24:42 -0400

    Update FireSpell.java

--- FireSpell.java
+++ FireSpell.java
@@ -1,4 +1,6 @@
 public class FireSpell extends Spell{
+    //Creates an instance of FireSpell item
+    //Provides function to allow for usage of the spell
     public FireSpell(String n, double p, long l, double d, double mp) {
         super(l, p, n, "Fire Spell", d, mp);
     }

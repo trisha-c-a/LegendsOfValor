1121d3a89388d20c3b959f165f9805be6af49bc1
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:07:59 -0400

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -1,4 +1,6 @@
 public class Armor extends Item{
+    // The damageReduction is used to calculate the amount of damage taken after equiping the armor
+    // Armor constructor takes the super class Item constructor to set the price and level requirements and adds damageReduction attributes to it
 
     public long damageReduction;
     public Armor(String n,  double p, long l, long dR) {

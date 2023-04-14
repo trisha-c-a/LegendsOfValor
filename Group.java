b65624098cc2fdb74dbff521879b3876a5be5d2d
desmondpun <84723289+desmondpun@users.noreply.github.com>
2023-04-14 17:30:31 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -1,4 +1,6 @@
 public interface Group {
+    //This interface makes sure the HeroGroup class and MonsterPack class implement the following methods
+    //These methods helps check the current status of the characters inside the groups
 
     public void stats();
 

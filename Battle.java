7f89eb5b76ee1cbfbeae4167b3be1c895ba252a8
Trisha Celine Anil <56964258+trisha-c-a@users.noreply.github.com>
2023-04-14 20:11:01 -0400

    Delete Battle.java

--- Battle.java
+++
@@ -1,8 +0,0 @@
-public interface Battle {
-    // ensures the classes that implements this has the ability to start fight and check winning condition
-
-    public boolean Fight(HeroGroup heroes);
-
-    public boolean hasWon(HeroGroup heroes, MonsterPack monsters);
-
-}
